fa47300a06bc5d7eb86561884afa479dad07aabd
Rick E <codeblock@eighthbit.net>
2009-08-17 03:11:05 -0400

    First commit of FreeBSDbot. PR Module kind-of works, but throws a warning right now. Disabling warnings works for now until I work out the bug.

---
+++ freebsd.pl
@@ -0,0 +1,31 @@
+#!/usr/local/env perl
+use warnings;
+use strict;
+
+package FreeBSD;
+use base qw( Bot::BasicBot );
+
+sub said{
+	my $self	= shift;
+	my $info	= shift;
+	my $text	= $info->{body};
+	my $nick	= $info->{who};
+	my $mask	= $info->{raw_nick};
+	my $channel	= $info->{channel};
+	
+	if($text =~ /^!test$/i){
+		$self->reply($info,"Y HALO THAR, $nick! LAWL.");
+	}
+
+}
+
+FreeBSD->new(
+	server		=> "cesium.eighthbit.net",
+	port		=> 6667,
+	channels	=> ["#offtopic"],
+	nick		=> "FreeBSD",
+	alt_nicks	=> ["BSDbot","BSD"],
+	username	=> "FreeBSD",
+	name		=> "FreeBSD Information Bot.",
+	charset		=> "utf-8"
+)->run();
